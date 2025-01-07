ca18ce0f4e747cc3bfe4758dd8c8516d93e15ead
Inthetouch <nikita_eremeev@mail.ru>
2025-01-07 18:42:52 +0300

    4 stage

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'taskidb', # имя базы данных
+        'USER': 'admin', # имя пользователя
+        'PASSWORD': 'admin', # пароль
+        'HOST': 'db',  # имя сервиса базы данных в docker-compose
+        'PORT': '5432', # порт для подключения
     }
 }
 
